9e4eea279fc8f5d3c23fc24246666af21392282d
anu5priya7 <anu5priya7@gmail.com>
2023-10-20 00:11:43 +0530

    Binary Search

--- Binarysearch.java
+++ Binarysearch.java
@@ -25,4 +25,34 @@ public class Binarysearch {
     }
 }
     
+//----------------using recursion---------------------
+
+// class Solution {
+   
+//         int binarysearch(int arr[], int n, int k) {
+//         // code here
+//         int low=0,high=n-1;
+//         return BinarySearch(arr,k,low,high);
+
+//         }
+        
+//     int BinarySearch(int array[], int x, int low, int high) {
+//         if (high >= low) {
+//             int mid = low + (high - low) / 2;
+
+//     // If found at mid, then return it
+//         if (array[mid] == x)
+//             return mid;
+
+//     // Search the left half
+//         if (array[mid] > x)
+//             return BinarySearch(array, x, low, mid - 1);
+
+//     // Search the right half
+//         return BinarySearch(array, x, mid + 1, high);
+//     }
+
+//     return -1;
+//     }
+// }
 
