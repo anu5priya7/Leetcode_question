dcae6ec15ae4f58121048d75fce3195bc9e07aed
anu5priya7 <anu5priya7@gmail.com>
2023-10-19 23:19:26 +0530

    first commit

---
+++ anu.java
@@ -0,0 +1,42 @@
+ 
+import java.io.*;
+import java.util.*;
+
+class anu{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.missingNumber(array, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int missingNumber(int array[], int n) {
+        // Your Code Here
+        
+      int ans = (n*(n+1))/2;
+      int add =0;
+      for(int i=0; i<array.length; i++){
+          add = add+array[i];
+      }
+      
+      int sum = ans-add;
+      return sum;
+    }
+
+}
